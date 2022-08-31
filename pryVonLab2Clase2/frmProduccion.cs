9e8a83b956f9e29c3fc28ef7c06095b1e24cb64d
nicovon24 <nicovon24@gmail.com>
2022-08-31 17:05:52 -0300

    Adding the flags which will not allow put more than once the data in production

--- pryVonLab2Clase2/frmProduccion.cs
+++ pryVonLab2Clase2/frmProduccion.cs
@@ -20,40 +20,64 @@ namespace pryVonLab2Clase2
 
         private void frmProduccion_Load(object sender, EventArgs e)
         {
-            StreamReader locationsReader = new StreamReader("./localidades.txt");
-            StreamReader cultivosReader = new StreamReader("./cultivos.txt");
+            StreamReader srLocations = new StreamReader("./localidades.txt");
+            StreamReader srCultivos = new StreamReader("./cultivos.txt");
             
-            char separador = Convert.ToChar(",");
+            char separador = Convert.ToChar(","); //split only supports char data
 
-            while (!locationsReader.EndOfStream) {
-                string[] location = locationsReader.ReadLine().Split(separador);
-                cbLocalidades.Items.Add(location[0]);
+            while (!srLocations.EndOfStream) {
+                string[] arrLocations = srLocations.ReadLine().Split(separador);
+                cbLocalidades.Items.Add(arrLocations[0]); //adding to the combo box only the location names from the location file
             }
-            while (!cultivosReader.EndOfStream)
+            while (!srCultivos.EndOfStream)
             {
-                string[] cultivos = cultivosReader.ReadLine().Split(separador);
-                cbCultivos.Items.Add(cultivos[0]);
+                string[] arrCultivos = srCultivos.ReadLine().Split(separador);
+                cbCultivos.Items.Add(arrCultivos[0]); //adding only the names of the cultivos
             }
-            locationsReader.Close();
-            cultivosReader.Close();
+            srLocations.Close(); //ALWAYS REMEMBER TO CLOSE THE FILES!!!    
+            srCultivos.Close();
         }
 
         private void btnAceptar_Click(object sender, EventArgs e)
         {
-            string msg = "";
-            /* ADD THE DATA!!! */
+            //stream readers
+            StreamReader srProduccion = new StreamReader("./produccion.txt");
+
+            //variables
             DateTime varDate = dateDate.Value.Date;
             string localidad = cbLocalidades.Text;
             string cultivo = cbCultivos.Text;
             int toneladas = Convert.ToInt32(nudToneladas.Text);
-            if(localidad!="" && cultivo!="" && toneladas != 0)
+            bool flag = false; //we check if we have data for the location in the 'x' cultivo, if we already have, we will not add it
+            char separador = Convert.ToChar(",");
+
+            //flags
+            while (!srProduccion.EndOfStream)
             {
-                msg = "Localidad: " + localidad + " | " + "Cultivo: " + cultivo + " | " + "Toneladas: " + toneladas + " | Fecha " + varDate;
-                using (StreamWriter sw = File.AppendText("./produccion.txt"))
-                {
-                    sw.WriteLine(msg);
+                string[] arrProduccion = srProduccion.ReadLine().Split(separador);
+                //we can not add the cultivo from the locations twice or more times
+                if (localidad == arrProduccion[0] && cultivo == arrProduccion[1]) { 
+                    flag = true; 
                 }
-                MessageBox.Show("Added data");
+            }
+
+            srProduccion.Close();
+
+            //program
+            if (localidad != "" && cultivo != "" && toneladas != 0)
+            {
+                if (flag==false) {
+                    string msg = localidad + "," + cultivo + "," + toneladas + "," + varDate;
+                    using (StreamWriter sw = File.AppendText("./produccion.txt"))
+                    {
+                        sw.WriteLine(msg);
+                    }
+                    MessageBox.Show("Added data");
+                }
+                else
+                {
+                    MessageBox.Show("Can not add this data, it is already the location and cultivo data in the file.");
+                }
             }
             else { MessageBox.Show("Complete the data, please!"); }
         }
